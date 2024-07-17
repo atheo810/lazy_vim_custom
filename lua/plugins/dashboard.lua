42bc59b7c8243affa70cacaed611c0034529b63a
atheo810 <atheos810@gmail.com>
2024-07-17 09:36:12 +0800

    feat: modularize update

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,10 +1,1 @@
-return {
-  "nvimdev/dashboard-nvim",
-  event = "VimEnter",
-  config = function()
-    require("dashboard").setup({
-      -- config
-    })
-  end,
-  dependencies = { { "nvim-tree/nvim-web-devicons" } },
-}
+return {}
--- lua/plugins/ui.mini-starter.lua
+++ lua/plugins/ui.mini-starter.lua
@@ -1,2 +1,2 @@
 -- use mini.starter instead of alpha
-return { import = "lazyvim.plugins.extras.ui.mini-starter" }
+return { import = "lazyvim.plugins.extras.ui.mini-starter", enabled = false }
